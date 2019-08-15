83370f912f8423b96af59aa7b8385ddcd95d69f4
Jeff-ust <53845744+Jeff-ust@users.noreply.github.com>
2019-08-15 21:17:35 +0800

    upload Q2

---
+++ L5/L5Q2.py
@@ -0,0 +1,51 @@
+# Tic-Tac-Toe
+
+def printcell(cells):
+    print("-" * 13)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            print("| %s " % cells[i][j], end="")
+        print("|")
+        print("-" * 13)
+
+
+def check_col(cells):
+    for i in range(0, 3):
+        if cells[0][i] == cells[1][i] == cells[2][i] != ' ':
+            return True
+    return False
+
+
+def check_row(cells):
+    for i in range(0, 3):
+        if cells[i][0] == cells[i][1] == cells[i][2] != ' ':
+            return True
+    return False
+
+
+def check_diagonal(cells):
+    if cells[0][0] == cells[1][1] == cells[2][2] != ' ':
+        return True
+    if cells[0][2] == cells[1][1] == cells[2][0] != ' ':
+        return True
+    return False
+
+
+def check(cells):
+    if check_col(cells) or check_row(cells) or check_diagonal(cells):
+        return True
+    return False
+
+
+cells = [[' ',' ',' '], [' ',' ',' '], [' ',' ',' ']]
+printcell(cells)
+while True:
+    col = int(input("Please enter column\t"))
+    row = int(input("Please enter row\t"))      
+    if cells[row][col] != ' ':        
+        print("It is taken already.")
+    cells[row][col] = "O"
+    printcell(cells)
+    if check(cells) == True:
+        print("You win!")
+        break
